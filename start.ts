64d5b46d566fda604b3f24499f8728ad5c07635b
Mario Zito <mario.zito@treme.io>
2024-04-21 22:35:22 -0300

    Fix start for running diffrente examples

--- start.ts
+++ start.ts
@@ -1,45 +1,8 @@
-import path from "path";
-import config from "./deploy.config";
-import { LocalCloud, JobData, zkCloudWorker } from "zkcloudworker";
 
-export async function main(args: string[]) {
-  const projectDir = path.join(config.workersDir, args[0]);
-
-  console.log("Executing zkCloudWorker code...");
-  const currDir = process.cwd();
-  const workerDir = path.join(path.dirname(currDir), projectDir);
-  console.log(`Worker dir: ${workerDir}`);
-
-  const functionName = "zkcloudworker";
-  const timeCreated = Date.now();
-  const job: JobData = {
-    id: "local",
-    jobId: "jobId",
-    developer: "@dfst",
-    repo: "simple-example",
-    task: "example",
-    userId: "userId",
-    args: Math.ceil(Math.random() * 100).toString(),
-    metadata: "simple-example",
-    txNumber: 1,
-    timeCreated,
-    timeCreatedString: new Date(timeCreated).toISOString(),
-    timeStarted: timeCreated,
-    jobStatus: "started",
-    maxAttempts: 0,
-  } as JobData;
-  
-  console.log("Importing worker from:", workerDir);
-  const zkcloudworker = await import(workerDir);
-  console.log("Getting zkCloudWorker object...");
-  
-  const cloud = new LocalCloud({ job, chain: "local", localWorker: zkcloudworker });
-
-  const worker = await zkcloudworker[functionName](cloud);
-  console.log("Executing job...");
-  console.log("Job:", JSON.stringify(job, null, 2));
-  const result = await worker.execute();
-  console.log("Job result:", result);
+async function main(args: string[]) {
+  const name = args[0];
+  const { run } = await import(`./src/${name}`);
+  await run([name]);
 }
 
 main(process.argv.slice(2)).catch((error) => {
